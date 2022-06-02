f39c2308604384c27f0f0f6444f4b9ae5efe2c18
Arya_Shanish <70702833+Aryashanish@users.noreply.github.com>
2022-06-02 22:23:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing", {
+    strings: ["Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: ["Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
